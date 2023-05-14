4ecde4964e501a9a8313cbc386e240b347c131be
Plotnikov Aleksandr <shukik85@ya.ru>
2023-05-14 13:21:31 +0300

    Add files via upload

---
+++ Learn2_16.php
@@ -0,0 +1,48 @@
+<?php
+define('PARAGRAF', str_repeat("<br>", 2));
+
+print_r("<b>Домашняя работа по итогам 16 урока.</b>");
+echo PARAGRAF;
+
+$link = mysqli_connect("localhost", "root", "", "Learn2_15");
+
+if ($link == false) {
+	echo "<br>Подключиться не удалось." . mysqli_connect_error($link);
+}
+
+$sql = "SELECT orderDate FROM orders LIMIT 1";
+
+$result = mysqli_query($link, $sql);
+
+$arr = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+print_r("<br>Список заказов совершенных: " . "<b>" . $arr['orderDate'] . ".</b>");
+
+echo PARAGRAF;
+
+$sql = "SELECT \n
+
+	orders.ID, clients.surname, clients.firstname, clients.secondname,\n
+
+	clients.phone, products.productName, materials.materialName\n
+
+    FROM clients\n
+
+    JOIN orders ON orders.clientID = clients.ID\n
+
+    JOIN materials ON orders.materialID = materials.ID\n
+
+    JOIN products ON orders.productID = products.ID\n
+
+    WHERE orders.orderDate = \"" . $arr['orderDate'] . "\"";
+
+if ($result = mysqli_query($link, $sql)){
+	while($arr = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+		print_r("ID = ");
+		foreach($arr as $key => $value){
+			print_r($value . "<br>");
+		}
+		print_r(PARAGRAF);
+	}
+}
+mysqli_close($link);
