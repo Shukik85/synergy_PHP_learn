c59b285ca5e0c78f58a4992fb7a0474b7407fcd2
Plotnikov Aleksandr <shukik85@ya.ru>
2023-05-14 21:17:12 +0300

    Signed-off-by: Plotnikov Aleksandr <shukik85@ya.ru>
    ok

--- Learn2_11.php
+++ Learn2_11.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 define ("PARAGRAF", str_repeat("<br>", 3));
@@ -17,24 +16,4 @@ print_r (date("d-m-Y", strtotime($str_date)) . BR);
 echo PARAGRAF;
 
 $new_year = "01/01/23";
-=======
-<?php
-
-define ("PARAGRAF", str_repeat("<br>", 3));
-define('BR', '<br>');
-
-$birthday = mktime(0,0,0,22,11,23);
-$today = time();
-const SECOND_PER_DAY = 60*60*24;
-print_r ("До дня рождения осталось: " . intval(($birthday - $today)/SECOND_PER_DAY) . " дней." . BR);
-
-echo PARAGRAF;
-
-$str_date = "2022-02-23";
-print_r (date("d-m-Y", strtotime($str_date)) . BR);
-
-echo PARAGRAF;
-
-$new_year = "01/01/23";
->>>>>>> de26b65 (Signed-off-by: Plotnikov Aleksandr <shukik85@ya.ru>)
 print_r ("С нового года прошло: " . intval((time() - strtotime($new_year))/SECOND_PER_DAY) . "дней!" . BR);
--- Learn2_16.php
+++ Learn2_16.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 define('PARAGRAF', str_repeat("<br>", 2));
 
@@ -46,58 +45,5 @@ if ($result = mysqli_query($link, $sql)){
 		print_r(PARAGRAF);
 	}
 }
-=======
-<?php
-define('PARAGRAF', str_repeat("<br>", 2));
-
-print_r("<b>Домашняя работа по итогам 16 урока.</b>");
-echo PARAGRAF;
-
-$link = mysqli_connect("localhost", "root", "", "Learn2_15");
-
-if ($link == false) {
-	echo "<br>Подключиться не удалось." . mysqli_connect_error($link);
-}
-
-$sql = "SELECT orderDate FROM orders LIMIT 1";
-
-$result = mysqli_query($link, $sql);
-
-$arr = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-print_r("<br>Список заказов совершенных: " . "<b>" . $arr['orderDate'] . ".</b>");
-
-echo PARAGRAF;
-
-$sql = "SELECT \n
-
-	orders.ID, clients.surname, clients.firstname, clients.secondname,\n
-
-	clients.phone, products.productName, materials.materialName\n
-
-    FROM clients\n
-
-    JOIN orders ON orders.clientID = clients.ID\n
-
-    JOIN materials ON orders.materialID = materials.ID\n
-
-    JOIN products ON orders.productID = products.ID\n
-
-    WHERE orders.orderDate = \"" . $arr['orderDate'] . "\"";
-
-if ($result = mysqli_query($link, $sql)){
-	while($arr = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-		print_r("ID = ");
-		foreach($arr as $key => $value){
-			print_r($value . "<br>");
-		}
-		print_r(PARAGRAF);
-	}
-}
-<<<<<<< HEAD
->>>>>>> de26b65 (Signed-off-by: Plotnikov Aleksandr <shukik85@ya.ru>)
-mysqli_close($link);
-=======
 mysqli_close($link);
 
->>>>>>> a61c6c7 (Signed-off-by: Plotnikov Aleksandr <shukik85@ya.ru>)
